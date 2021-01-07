c82f32cb11a949472e0cb4ca4cda77f411aa45b2
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-07 06:55:31 -0600

    removed unused log and comments

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -15,7 +15,6 @@ const Nav = styled.nav`
 `;
 
 const Navbar = () => {
-  console.log('NavBar');
   return (
     <Nav>
       <div>
@@ -27,79 +26,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// import { Link, navigate } from 'gatsby';
-// import React from 'react';
-// import styled from 'styled-components';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {
-//   faHome,
-//   faSignInAlt,
-//   faSignOutAlt,
-//   faUserPlus,
-//   faUserCircle,
-//   faUsers,
-//   faBars,
-// } from '@fortawesome/free-solid-svg-icons';
-// import { isLoggedIn, logout } from '../../services/auth';
-
-// const Header = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: flex-start;
-
-// span{
-// color: #3D3D3D;
-// font-size: 6px;
-// opacity: 0;
-// }
-// /* .icon {
-//   color: lightgrey;
-//   display: none;
-// } */
-// //   }
-
-// //   .brand {
-// color: var(--green);
-// font-family: 'Aleo';
-// font-weight: bold;
-// //   }
-
-// //   .nav-wrapper {
-// display: flex;
-// //   }
-
-// //   .icon-wrapper {
-// display: flex;
-// flex-direction: column;
-// align-items:center;
-// font-family: Open Sans;
-// padding-right: 15px;
-// //   }
-
-// //   .icon {
-// display: none;
-// //   }
-
-// //   @media screen and (max-width: 600px) {
-// .nav-wrapper a {display: none;}
-// .nav-wrapper a.icon {
-//   display: block;
-// }
-// //   }
-// // `;
-
-// // const NavBar = () => {
-// //   const iconBar = document.querySelector('.nav-wrapper');
-// //   console.log({ iconBar });
-// //   return (
-// <Header>
-//   <div>
-//     <span className='brand'>THE FLAMING IGUANA</span>
-//   </div>
-
-// </Header>
-//   );
-// };
-
-// export default NavBar;
