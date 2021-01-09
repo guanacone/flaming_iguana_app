6d86903c10599a753c1eb20888e2a3a5f8157877
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-09 12:42:33 -0600

    removed global styles

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -4,19 +4,5 @@ const GlobalStyles = createGlobalStyle`
     :root {
         --green: #9ECF73;
     }
-    body {
-        font-family: Open Sans, Helvetica Neue, sans-serif,
-    }
-    
-    h1 {
-        text-align: center;
-        font: normal normal 300 40px/48px Open Sans;
-        letter-spacing: -0.6px;
-        color: #3D3D3D;
-    }
-
-    h3 {
-        color: red;
-    }
 `;
 export default GlobalStyles;
