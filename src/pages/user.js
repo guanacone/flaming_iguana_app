9a8b77c99cf27a6e3f6cffcbc3842790121e9e9d
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-16 10:23:18 -0600

    removed password reset from clients-only routes

--- src/pages/user.js
+++ src/pages/user.js
@@ -4,7 +4,6 @@ import UserIndex from '../components/UserIndex';
 import UserProfile from '../components/UserProfile';
 import UserEdit from '../components/UserEdit';
 import PasswordEdit from '../components/PasswordEdit';
-// import PasswordReset from './reset_password';
 
 const User = () => (
   <Router basepath='/user'>
@@ -12,7 +11,6 @@ const User = () => (
     <UserProfile path='/:id' />
     <UserEdit path='/:id/edit' />
     <PasswordEdit path='/:id/password_edit' />
-    {/* <PasswordReset path='/password_reset' /> */}
   </Router>
 );
 
