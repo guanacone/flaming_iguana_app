93b435e03fac541c77c32cda27e78e6a629d6a26
Gilles Rusca <gilles.rusca@gmail.com>
2021-01-18 10:52:40 -0600

    corrected footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterWrapper = styled.footer`
+ color: ${({ isProtected }) => (isProtected ? 'white' : '#909090')};
 
   h2 {
   font: normal normal 300 10px/14px Open Sans;
   letter-spacing: 0px;
-  color: #909090;
   text-align: center;
   opacity: 1;
   }
@@ -14,12 +14,12 @@ const FooterWrapper = styled.footer`
   a {
     font-weight: bold;
     text-decoration: underline;
-    color: #909090;
+    color: ${({ isProtected }) => (isProtected ? 'white' : '#909090')};
   }
 `;
 
-const Footer = () => (
-  <FooterWrapper>
+const Footer = ({ isProtected }) => (
+  <FooterWrapper isProtected={isProtected}>
     <h2>Copyright &#169; 2012 <a href='mailto:ryan@bandada.ca?subject=contact inquiry'>Ryan Hryciuk</a> & <a href='mailto:gilles@rusca.dev?subject=contact inquiry'>Gilles Rusca</a></h2>
   </FooterWrapper>
 );
