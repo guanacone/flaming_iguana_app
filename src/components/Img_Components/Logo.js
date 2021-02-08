8cff1bfb1f47dd20de23c6971fe7a171ed8bc23d
Gilles Rusca <gilles.rusca@gmail.com>
2021-02-08 09:05:08 -0600

    styled comp

--- src/components/Img_Components/Logo.js
+++ src/components/Img_Components/Logo.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const StyledImg = styled(Img)`
+  width: 20vw;
+  min-width: 200px;
+`;
 
 const Logo = () => {
   const data = useStaticQuery(graphql`
@@ -16,7 +22,7 @@ const Logo = () => {
   `);
 
   return (
-    <Img
+    <StyledImg
       className='logo'
       fluid={data.file.childImageSharp.fluid}
       alt='vectorized picture of an iguana head'
